30936f2a895bde9f7958a594bfdc6d0ee1914fbe
JimmyGrijpPlay <jgrijp@playav.be>
2025-03-15 22:10:45 +0100

    Update client.js

--- js/client.js
+++ js/client.js
@@ -1,5 +1,6 @@
 window.TrelloPowerUp.initialize({
   'card-buttons': function(t, options) {
+    console.log('Card buttons loaded');
     return [{
       icon: 'https://jimmygrijpplay.github.io/trello-label-updater/images/icon-light.png',
       text: 'Change Label-Id',
@@ -12,10 +13,18 @@ window.TrelloPowerUp.initialize({
       }
     }];
   },
-  'card-badges': function(t, options) {
+  'card-detail-badges': function(t, options) {
     return [{
-      text: 'Labels',
-      color: 'blue'
+      title: 'Label Manager',
+      text: 'Change Labels',
+      color: 'blue',
+      callback: function(t) {
+        return t.popup({
+          title: 'Change Label-Id of any card',
+          url: 'https://jimmygrijpplay.github.io/trello-label-updater/index.html',
+          height: 200
+        });
+      }
     }];
   }
 }); 
